49b8d72c4700b96095a01b4ee6637d9ee3ec0ddc
Christopher <christo_campiglia@hotmail.com>
2018-08-16 09:09:49 -0300

    [ADD] Reconfigured Server.

---
+++ source/server.js
@@ -0,0 +1,71 @@
+const HTTP = require ("http");
+const FileSystem = require ("fs");
+const {ConfigureFilePath, ConfigureHead} = require ("./response.js");
+
+
+class Server {
+
+	constructor (Options = {}) {
+
+		this.Options = {
+
+			RootFolder: Options.folder,
+			IndexFile: Options.index || "index.html",
+			Port: Options.port || 8000
+		};
+
+		this.Server = HTTP.createServer ((Request, Response) => {
+
+			const ReceivedRequest = Date.now ();
+
+
+			const FilePath = ConfigureFilePath (this.Options, Request.url);
+
+			console.log (`[${new Date ().toLocaleString ()}] | [...] ${Request.url}`);
+
+
+			FileSystem.readFile (FilePath, function (SomeError, Data) {
+
+				if (!SomeError) {
+
+					Response.writeHead (200, ConfigureHead (FilePath));
+					Response.end (Data, "utf-8");
+
+					console.log (`[${new Date ().toLocaleString ()}] | [200] ${Request.url} | [${Date.now () - ReceivedRequest}ms]`);
+				}
+				else {
+
+					if (SomeError.code === "ENOENT") {
+
+						Response.writeHead (404);
+						Response.end ();
+
+						console.log (`[${new Date ().toLocaleString ()}] | [404] ${Request.url}`);
+					}
+					else {
+
+						Response.writeHead (500);
+						Response.end ();
+
+						console.log (`[${new Date ().toLocaleString ()}] | ${Request.url}`);
+						console.log (`Internal Server Error: ${SomeError.code}`);
+					}
+				}
+			});
+		});
+	}
+
+	Start () {
+
+		this.Server.listen (this.Options.Port);
+		console.log (`Server running at port ${this.Options.Port}`);
+	}
+
+	Stop () {
+
+		console.log ("Server stoped")
+		process.exit ();
+	}
+}
+
+module.exports = Server;
