ee145015100f848adac544d34d4def3fa0c3eabc
Christopher <christo_campiglia@hotmail.com>
2018-08-16 15:22:20 -0300

    [UPDATE] Help Text.

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ const Commander = require ("commander");
 Commander
 	.option ("-f, --folder <folder>", "The folder where run the server")
 	.option ("-p, --port <port>", "The port where the server will listen")
-	.option ("-i, --index <index>", "The index file to be loaded when the user enters '/'")
+	.option ("-i, --index <index>", "The name of the file to be loaded when the user enters '/'")
 	.action (() => {
 
 		const MyServer = new Server (Commander);
