280b233d1fa530f8e7b71f28b2d4e3190610ef45
Christopher <christo_campiglia@hotmail.com>
2018-08-16 15:17:42 -0300

    [UPDATE] CLI Options.

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 const Server = require ("./../source/server.js");
+const Commander = require ("commander");
 
-const MyServer = new Server ();
-MyServer.Start ();
+Commander
+	.option ("-f, --folder <folder>", "The folder where run the server")
+	.option ("-p, --port <port>", "The port where the server will listen")
+	.option ("-i, --index <index>", "The index file to be loaded when the user enters '/'")
+	.action (() => {
+
+		const MyServer = new Server (Commander);
+		MyServer.Start ();
+	})
+	.parse (process.argv);
